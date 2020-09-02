495440a4a780097f13b92867a0582ac8b5af3df1
Alexander Wanyoike <wanyoike.alexander@gmail.com>
2020-09-02 11:39:12 +0100

    Temporary workaround for partial exports

--- Reader/Database/ProductModelReader.php
+++ Reader/Database/ProductModelReader.php
@@ -8,6 +8,18 @@ class ProductModelReader extends BaseProductModelReader
 {
     protected function getConfiguredFilters()
     {
+        if ($this->stepExecution->getJobExecution()->getJobInstance()->getJobName() === "partial_export") {
+            return [
+                [
+                    "field" => "updated",
+                    "operator" => "SINCE LAST JOB",
+                    "value" => $this->stepExecution
+                        ->getJobExecution()
+                        ->getJobInstance()
+                        ->getCode()
+                ]
+            ];
+        }
         return [];
     }
 }
